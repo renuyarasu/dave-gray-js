7b94cc94f52147d05ab783d438e4a5d5d24721e1
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-17 21:53:21 +0530

    Math.round

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.clear();
 
 // Math Methods and Properties
 const mathPi = Math.PI;
-const math = Math.trunc(Math.PI);
+const math = Math.round(3.8);
 
 console.log(mathPi);
 console.log(math);
