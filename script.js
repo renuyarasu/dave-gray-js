a6de92c5cf9a63b6e782df401465d61476ea88ef
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-17 21:45:53 +0530

    Math Methods and Properties

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours 
 console.clear();
 
-// Number Methods
-const myNumber = 7;
-const myString = '7.11yhh1j1j';
-const myFlootNumber = 7.017789;
-
-
-console.log(isNaN('2ss'));
+// Math Methods and Properties
 
