83b3ed18fb26677527d760a929d7a66a2b7c6aea
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-17 21:38:24 +0530

    Number.toFixed()

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ console.clear();
 
 // Number Methods
 const myNumber = 7;
-const myString = '7';
-const myFlootNumber = 7.017789020;
+const myString = '7.11yhh1j1j';
+const myFlootNumber = 7.017789;
 
 // Number.parseFloat()
 // The Number.parseFloat() method parses an argument and returns a floating point number. If a number cannot be parsed from the argument, it returns NaN.
 
-console.log(Number.parseFloat(myFlootNumber)); // true
+console.log(Number.parseFloat(myString).toFixed(4)); // 7.0178
 
