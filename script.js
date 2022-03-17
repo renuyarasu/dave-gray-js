464e5ba1135a57a3d99f9718cb7a609a42a5d1c4
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-17 20:49:22 +0530

    Strings: length

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+// JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours 
+console.clear();
+
+//Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+const myVariable = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
+console.log('JavaScript Full Course for Beginners'.length);
 
