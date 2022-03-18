c5e36eb6bc8cc8bcfffd37f8dfcad0bd86562081
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-18 20:37:19 +0530

    Switch Statements - Example 02

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ console.clear();
 
 // Conditionals: Switch Statements
 
-switch (Math.floor(Math.random() * 5 + 1)) {
-    case 1:
-        console.log('1');
+let playerOne = 'scissors';
+let computer = 'rock';
+
+switch (playerOne) {
+    case computer:
+        console.log('Tie game!');
+        break;
+    case 'rock':
+        if (computer === 'paper') {
+            console.log('Computer wins!');
+        } else {
+            console.log('Player one wins!');
+        }
         break;
 
-    case 2:
-        console.log('2');
+    case 'paper':
+        if (computer === 'scissors') {
+            console.log('Computer wins!');
+        } else {
+            console.log('Player one wins!');
+        }
         break;
-
-    case 3:
-        console.log('3');
-        break;
-
     default:
-        console.log('No Match');
+        if (computer === 'scissors') {
+            console.log('Computer wins!');
+        } else {
+            console.log('Player one wins!');
+        }
+
 }
