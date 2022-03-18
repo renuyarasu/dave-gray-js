7c3b5086a1b94541adc21dc89bda71def1f8d0c0
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-18 21:10:18 +0530

    User Input - alert()

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours 
 console.clear();
-
-// Conditionals: Ternary Operators
-//syntax
-// condition ? if true : if false
-// ':' else here
-
-let playerOne = 'rock';
-let computer = 'paper';
-let result =
-    playerOne === computer
-        ? 'Tie Game!'
-        : playerOne === 'rock' && computer === 'paper'
-            ? 'Computer Wins!'
-            : playerOne === 'paper' && computer === 'scissors'
-                ? 'Computer Wins!'
-                : playerOne === 'scissors' && computer === 'rock'
-                    ? 'Computer Wins!'
-                    : 'Player One Wins!';
-console.log(result);
-
+// User Input
+alert('Hello World!')
