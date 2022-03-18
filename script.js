7267bebeb904eed47d9d9e4295403596d341f518
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-18 20:44:09 +0530

    Ternary Operators - Example 01

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours 
 console.clear();
 
-// Conditionals: Switch Statements
+// Conditionals: Ternary Operators
+//syntax
+// condition ? if true : if false
+// ':' else here
 
-let playerOne = 'scissors';
-let computer = 'rock';
-
-switch (playerOne) {
-    case computer:
-        console.log('Tie game!');
-        break;
-    case 'rock':
-        if (computer === 'paper') {
-            console.log('Computer wins!');
-        } else {
-            console.log('Player one wins!');
-        }
-        break;
-
-    case 'paper':
-        if (computer === 'scissors') {
-            console.log('Computer wins!');
-        } else {
-            console.log('Player one wins!');
-        }
-        break;
-    default:
-        if (computer === 'scissors') {
-            console.log('Computer wins!');
-        } else {
-            console.log('Player one wins!');
-        }
-
-}
+// let soup = 'Chicken Noodle Soup.';
+let soup = false;
+let response = soup ? 'Yes! we have soup.' : 'No!, soup today!';
+console.log(response);
