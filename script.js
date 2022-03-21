a56cd44a857b2e9b7ecc531ea80eebdc1228132a
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-21 16:32:16 +0530

    Functions

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours 
 console.clear();
-// Loops
-// For Loops
 
-let name = 'vedagna';
-for (let i = 0; i < name.length; i++) {
-    console.log(name.charAt(i));
-}
-// v
-// e
-// d
-// a
-// g
-// n
-// a
+// Functions
+// Functions are one of the fundamental building blocks in JavaScript. A function in JavaScript is similar to a procedure—a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output. To use a function, you must define it somewhere in the scope from which you wish to call it.
+
+// Methods: Built-in Functions
+
