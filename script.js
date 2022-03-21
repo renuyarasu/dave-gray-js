39ab722f183ea3eba6f6bdb0d2260e130a7f2fd5
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-21 20:43:19 +0530

    Arrays: pop()

--- script.js
+++ script.js
@@ -10,5 +10,6 @@ myArray[1] = 5;
 myArray[2] = true;
 
 myArray.push('school'); //The push() method adds one or more elements to the end of an array and returns the new length of the array.
+myArray.pop('school'); //The pop() method removes the last element from an array and returns that element. This method changes the length of the array.
 
-console.log(myArray);
+console.log(myArray); // [ 'VedaGna', 5, true ]
