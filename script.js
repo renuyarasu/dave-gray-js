9382b061aa99f5887d6c5e5c69df6c5730d849b6
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-21 21:22:11 +0530

    Arrays: reverse()

--- script.js
+++ script.js
@@ -4,9 +4,5 @@ console.clear();
 // Arrays
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-/* const newArray = myArray.slice(2) // The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
-console.log(newArray); //[   3, 4, 5, 6, 7, 8, 9, 0]
- */
-
-const newArray = myArray.slice(0, 5)
-console.log(newArray); //[ 1, 2, 3, 4, 5]
+const newArray = myArray.reverse()
+console.log(newArray); //[ 0, 9, 8, 7, 6, 5, 4, 3, 2, 1]
