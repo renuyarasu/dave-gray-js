6c99a5fa5ed70a1294fca85cec6e228af31a493d
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-21 22:16:38 +0530

    Objects: destructuring

--- script.js
+++ script.js
@@ -10,19 +10,11 @@ const bands = {
     bass: 'John Paul',
     drums: 'John Bonham',
 }
-const keys = Object.keys(bands);
-const values = Object.values(bands);
 
-console.log(keys); // [ 'vocals', 'guitar', 'bass', 'drums' ]
-console.log(values); // [ 'Robert Plant', 'Jimmy Page', 'John Paul', 'John Bonham' ]
+// destructuring objects
 
-for (let job in bands) {
-    console.log(`On ${job}, its's ${bands[job]}!`);
-}
-// On vocals, its's Robert Plant!
-// On guitar, its's Jimmy Page!
-// On bass, its's John Paul!
-// On drums, its's John Bonham!
-
-delete bands.bass;
-console.log(bands.hasOwnProperty('bass')); //false
+const { vocals, guitar, bass, drums } = bands;
+console.log(vocals);
+console.log(guitar);
+console.log(bass);
+console.log(drums);
