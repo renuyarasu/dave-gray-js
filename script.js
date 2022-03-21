97214b87f725e6741669d25ca508b1e135987445
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-21 20:36:09 +0530

    Arrays

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+console.clear();
 
+// Arrays
+const myArray = [];
+
+// Add elements to an array
+myArray[0] = 'VedaGna';
+myArray[1] = 5;
+myArray[2] = true;
+
+console.log(myArray); //refer an array
+console.log(myArray[1]); // second element in array
+console.log(myArray.length);// length property
+console.log(myArray[myArray.length - 1]); // last element in array
