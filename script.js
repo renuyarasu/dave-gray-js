b108db51ba3210b0d258de3eb9f72bebf4d253e6
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-21 21:27:08 +0530

    Arrays: split()

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ console.clear();
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
 
-const newArray = myArray.join()
-console.log(newArray); // 1,2,3,4,5,6,7,8,9,0
+const newArray = myArray.join()// 1,2,3,4,5,6,7,8,9,0
+const splitArray = newArray.split(',')
+console.log(splitArray); // [ '1', '2', '3', '4','5', '6', '7', '8', '9', '0']
