29947fab88e3342783fbf00f803ae4d27abad2ab
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-22 19:49:46 +0530

    Classes: Example 01

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+console.clear();
 
+// Classes
+// Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are not shared with ES5 class-like semantics.
+
+class Pizza {
+    constructor() {
+        this.size = 'medium';
+        this.crust = 'original';
+    }
+    bake() {
+        console.log(`Baking a ${this.size} size ${this.crust} crust pizza!`);
+    }
+}
+const myPizza = new Pizza;
+myPizza.bake();
