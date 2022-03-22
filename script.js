780fc035d5b5df059496f9cedbbefd5f219a843a
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-22 19:53:00 +0530

    Classes: Example 01 - parameter

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ console.clear();
 // Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are not shared with ES5 class-like semantics.
 
 class Pizza {
-    constructor() {
+    constructor(pizzatype) {
+        this.type = pizzatype;
         this.size = 'medium';
         this.crust = 'original';
     }
     bake() {
-        console.log(`Baking a ${this.size} size ${this.crust} crust pizza!`);
+        console.log(`Baking a ${this.size} size ${this.crust} crust ${this.type} pizza!`);
     }
 }
-const myPizza = new Pizza;
+const myPizza = new Pizza('pepper'); // Baking a medium size original crust pepper pizza!
+
 myPizza.bake();
