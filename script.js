699bb9b1b6e5c522002e9ff8950b7b7365a71619
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 15:18:50 +0530

    Classes: Example 01 - extends

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ console.clear();
 // JavaScript Classes
 // Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are not shared with ES5 class-like semantics.
 
-class Pizza {
-    constructor(typePizza, sizePizza) {
-        this.type = typePizza;
-        this.size = sizePizza;
+class Pizz {
+    constructor(pizzaSize) {
+        this.size = pizzaSize;
         this.crust = 'original';
-        this.toppins = [];
     }
     getCrust() {
         return this.crust;
@@ -17,22 +15,16 @@ class Pizza {
     setCrust(pizzaCrust) {
         this.crust = pizzaCrust;
     }
-    getToppins() {
-        return this.toppins;
-    }
-    setToppins(toppins) {
-        this.toppins.push(toppins)
-    }
-    bake() {
-        console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`);
-    }
 }
 
-const myPizza = new Pizza('small', 'pepper');
-myPizza.setCrust('thin');
-myPizza.bake();
-myPizza.setToppins('sauages');
-myPizza.setToppins('olives');
-
-console.log(myPizza.getCrust());
-console.log(myPizza.getToppins());
+class SpecialtyPizza extends Pizz {
+    constructor(pizzaSize) {
+        super(pizzaSize);
+        this.type = 'The Works';
+    }
+    slice() {
+        console.log(`Our ${this.type} ${this.size} pizza has 8 slices!`);
+    }
+}
+const myPizza = new SpecialtyPizza('medium');
+myPizza.slice();
