39f71b622b833fdb93048fcea691a16dcf906ef0
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 17:04:38 +0530

    Handling Errors: SyntaxError

--- script.js
+++ script.js
@@ -3,13 +3,9 @@
 
 // JavaScript Errors and Handling Errors
 
-// Uncaught ReferenceError: 
+// Uncaught SyntaxError: Unexpected token '.'
 
 "use strict"
 
-/* myName = 'VedaGna';
-console.log(myName); */
-
-var myName = 'VedaGna';
-console.log(myName);
+Object..create();
 
