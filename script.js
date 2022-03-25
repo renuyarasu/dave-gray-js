d427f2861e4fee5503f65e72fffb5450a95b9055
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 18:38:48 +0530

    DOM: Document Object Model

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+console.clear();
+// Document Object Model (DOM - https://www.youtube.com/watch?v=WbG86sMd3SU&list=PL0Zuz27SZ-6Oi6xNtL_fwCrwpuqylMsgT&index=21)
+/* 
+The Document Object Model: DOM
+Selecting an element with an ID
+Selecting an element with a query selector
+Changing the CSS with the style property
+Selecting elements by class name 
+Selecting elements with querySelectorAll()
+HTMLCollection vs NodeList
+Searching within the results of a previous selector
+Selecting elements by tag name
+Applying more specific DOM selectors
+Modifying CSS with a DOM selector
+Changing text inside of an element
+Changing the HTML within a parent element
+Navigating the DOM tree
+Removing all the nodes in a parent element
+Creating new elements
+Adding new property values to the element
+Appending the new element to the DOM
+Creating new elements with a loop */
+
 
