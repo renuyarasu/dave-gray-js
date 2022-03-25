f3de0976a31b49137ee9f74be1d838ea9e0b434f
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 16:03:18 +0530

    JSON

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+console.clear();
+// JSON | JavaScript Object Notation
+/*
+In this article, we'll look at fundamental JavaScript object syntax, and revisit some JavaScript features that we've already seen earlier in the course, reiterating the fact that many of the features you've already dealt with are objects.
+JSON is used to send and recieve dada.
+JSON is a text format that is completly language dependent.
+Meaning JSON is used to send & receive data in may languages (csharp, python,...)
+*/
 
+const myObject = {
+    name: 'VedaGna',
+    hobbies: ['eat', 'sleep', 'code'],
+    hello: function () {
+        console.log('Hello World!');
+    }
+};
+
+// console.log(myObject);
+console.log(myObject.name);
+myObject.hello();
+console.log(typeof myObject);
