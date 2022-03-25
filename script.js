b910f0bb518f5b0cf1bbe6087a86bd5ff346dd78
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 21:52:08 +0530

    Event Listeners

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+console.clear();
+/* // JavaScript: Event Listeners
+=================================
+Insure the DOM is available to interact with
+How to load your script for DOM interaction
+Syntax: addEventListener(event, function, useCapture)
+Using a function name in the event listener
+Removing a function with a name: removeEventListener()
+Using an anonymous function in the event listener
+Listening for the readystatechange event
+Constructing an initApp() function
+Event Bubbling
+Event Capturing
+Event Propagation and stopPropagation()
+event.target usage
+classList, add, remove, and toggle
+Listening for a HTML Form submit event
+HTML Form default behavior
+Prevent default behavior with event.preventDefault() */
+
+
 
