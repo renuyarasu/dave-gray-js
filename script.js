2f9de4c404a0e2c124a029eae27196f1ae6058db
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 17:46:49 +0530

    Handling Errors: try(), catch(), finally()

--- script.js
+++ script.js
@@ -18,21 +18,22 @@ when attempting to use a value in an inappropriate way. */
 "use strict"
 
 const makeError = () => {
-    try {
-        throw new customError('This is a custom error')
-    } catch (err) {
-        /* console.warn(err);
-        console.table(err);
-        console.error(err);*/
-        console.error(err.name);
-        console.error(err.message); 
-        console.error(err.stack);
+    let i = 1;
+    while (i <= 5) {
+        try {
+            if (i % 2 !== 0) {
+                throw new Error('Odd numbers!')
+            } 
+            console.log('Even numbers!');
+        } catch (err) {
+            console.error(err.name);
+            console.error(err.message);
+            console.error(err.stack);
+        } finally {
+            console.log('...finally');
+            i++;
+        }
     }
-}
+};
 makeError();
 
-function customError() {
-    this.message = this.message;
-    this.name = customError;
-    this.stack = `${this.name} ${this.message}`;
-}
