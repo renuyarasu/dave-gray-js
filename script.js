1c332906f35b8aa62b1e55433c0b1dad9ff4e3af
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 17:03:19 +0530

    Handling Errors: ReferenceError

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours 
-console.clear();
+// console.clear();
 
 // JavaScript Errors and Handling Errors
+
+// Uncaught ReferenceError: 
+
+"use strict"
+
+/* myName = 'VedaGna';
+console.log(myName); */
+
+var myName = 'VedaGna';
+console.log(myName);
+
