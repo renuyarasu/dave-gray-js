dd60c850e6e121729be63f31f99d0c5ac051e51d
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 13:53:42 +0530

    Classes: Example 01 - Dot Notation

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
 console.clear();
 
-// Classes
+// JavaScript Classes
 // Classes are a template for creating objects. They encapsulate data with code to work on that data. Classes in JS are built on prototypes but also have some syntax and semantics that are not shared with ES5 class-like semantics.
 
 class Pizza {
-    constructor(pizzatype) {
-        this.type = pizzatype;
-        this.size = 'medium';
+    constructor(typePizza, sizePizza) {
+        this.type = typePizza;
+        this.size = sizePizza;
         this.crust = 'original';
     }
     bake() {
-        console.log(`Baking a ${this.size} size ${this.crust} crust ${this.type} pizza!`);
+        console.log(`Baking a ${this.size} ${this.type} ${this.crust} crust pizza.`);
     }
 }
-const myPizza = new Pizza('pepper'); // Baking a medium size original crust pepper pizza!
-
+const myPizza = new Pizza('small', 'pepper');
+myPizza.type = 'large';
 myPizza.bake();
