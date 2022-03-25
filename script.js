b029032162593e481ba5b37ff7ad586b11fa2b9c
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-25 17:06:07 +0530

    Handling Errors: TypeError

--- script.js
+++ script.js
@@ -3,9 +3,21 @@
 
 // JavaScript Errors and Handling Errors
 
-// Uncaught SyntaxError: Unexpected token '.'
+// Uncaught TypeError: Assignment to constant variable.
+
+/* 
+The TypeError object represents an error when an operation could not be performed, typically (but not exclusively) when a value is not of the expected type.
+
+A TypeError may be thrown when:
+
+an operand or argument passed to a function is incompatible with the type expected by that operator or function; or
+when attempting to modify a value that cannot be changed; or
+when attempting to use a value in an inappropriate way. */
+
 
 "use strict"
 
-Object..create();
+const myName = 'VedaGna';
+myName = 'Gnapika';
+console.log(myName);
 
