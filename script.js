e177845453b24d8bb12f52157009c06d483dff8a
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-28 20:58:36 +0530

    Modules

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
 
+/* JavaScript Modules
+// https://www.theodinproject.com/ && https://www.freecodecamp.org
+// https://youtu.be/Q3SBogjUfMk?list=PL0Zuz27SZ-6Oi6xNtL_fwCrwpuqylMsgT
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
+Intro
+ES6 Module Basics
+JS Modules are only supported on servers
+Use the Live Server extension in VS Code
+The script tag with type="module"
+No defer needed
+Strict mode by default
+Create a Javascript Module
+Export default
+Export { firstItem, secondItem }
+Export inline
+Import the default export
+Import { firstItem, secondItem }
+Rename imports
+Import the full namespace
+JavaScript Class export and import example
+Current browser support for modules */
+'use strict';
+
+
+
