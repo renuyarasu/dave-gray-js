c80cf8eb690a030e21b899bacadd7e7ffca80f31
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-28 21:06:10 +0530

    Modules: export default

--- guitars.js
+++ guitars.js
@@ -1,2 +1,8 @@
 // Guitars
 
+const playGuitar = () => 'Playing Guitar...';
+const shredding = () => 'Shredding some licks...';
+const plucking = () => 'Plucking the strings...';
+
+export default playGuitar;
+export { shredding, plucking }
