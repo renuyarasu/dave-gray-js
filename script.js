324d9c73749531a09c43adc7851661f277ae92f2
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-31 21:40:26 +0530

    Fetch API: get joks from api.icndb.com

--- script.js
+++ script.js
@@ -3,12 +3,38 @@ console.clear();
 // Fetch API
 // Callbacks | Promises / Thenables / Async & Await
 
-// 2nd parameter of Fetch is a object
+// abstract into function
 
-const requestJoke = async (firstName, lastName) => {
-    const response = await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`);
-    const jsonResponse = await response.json();
-    const getJoke = jsonResponse.value.joke;
-    console.log(getJoke);
+// from Form
+const getDataFromForm = () => {
+    const requestObj = {
+        firstName: 'Bruce',
+        lastName: 'Lee',
+        categories: ['nerdy']
+    };
+    return requestObj;
 }
+
+const buildRequestUrl = (requestData) => {
+    return `http://api.icndb.com/jokes/random?firstName=${requestData.firstName}&lastName=${requestData.lastName}&limitTo=${requestData.categories}`;
+}
+
+const requestJoke = async (url) => {
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    const joke = jsonResponse.value.joke;
+    postJokeToPage(joke);   
+}
+const postJokeToPage = (joke) => {
+    console.log(joke);
+}
+
+// procedural 'workflow' function
+const processJokeRequst = async () => {
+    const requestData = getDataFromForm();
+    const requestUrl = buildRequestUrl(requestData);
+    await requestJoke(requestUrl);
+    console.log('Finished!');
+}
+processJokeRequst();
 requestJoke('Bruce', 'Lee'); // Bruce Lee once shat blood - the blood of 11,940 natives he had killed and eaten.
