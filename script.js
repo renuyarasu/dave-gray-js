502d4ead3aea745c2cb595f9bb966b0c4da5f8f7
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-31 20:04:12 +0530

    Fetch API: Promises - response.json()

--- script.js
+++ script.js
@@ -13,5 +13,9 @@ console.log(users);
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => {
-        console.log(response);
+        // console.log(response);
+        return response.json();
+    })
+    .then(data=>{
+        console.log(data);
     })
