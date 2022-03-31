263138d6442bc2119de73873f70c7cbcf3e18d58
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-31 19:58:40 +0530

    Fetch API: Promises - pending state

--- script.js
+++ script.js
@@ -6,25 +6,7 @@ console.clear();
 // Promises
 // 3 states: Pending, Fulfilled, Rejected
 
-const myPromise = new Promise((res, rej) => {
-    const error = false;
-    if (!error) {
-        res('Yes! resolved the promise.')
-    } else {
-        rej('No! rejected the promise.')
-    }
-});
+const users = fetch('https://jsonplaceholder.typicode.com/users');
 
-const myNewPromise = new Promise((res, rej) => {
-    setTimeout(() => {
-        res('myNewPromise! resolved the promise.')
-    }, 2000);
-});
-
-myNewPromise.then(value => {
-    console.log(value);
-})
-
-myPromise.then(value => {
-    console.log(value);
-})
+// Pending
+console.log(users);
