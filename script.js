1ae82e8a1aacda9d6ee4deec77130ae3a6f72e1e
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-04 22:36:18 +0530

    Regular Expressions: Adding Variables

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours
+console.clear();
+//Regular Expressions (https://regexr.com/)
 
+document.getElementById('phoneNum').addEventListener('input', (event)=>{
+    const reqex = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+    const input = document.getElementById('phoneNum');
+    const format = document.querySelector('.phoneFormat');
+    const phone = input.value;
+    const found = reqex.test(phone);
+})
