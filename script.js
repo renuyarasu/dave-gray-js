1bbe27cb8e7cea77eb3b07f5f1c590d802739344
Renuka Prasad <renu.yarasu@gmail.com>
2022-04-04 22:41:07 +0530

    Regular Expressions: Validation

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 console.clear();
 //Regular Expressions (https://regexr.com/)
 
-document.getElementById('phoneNum').addEventListener('input', (event)=>{
+document.getElementById('phoneNum').addEventListener('input', (event) => {
     const reqex = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
     const input = document.getElementById('phoneNum');
     const format = document.querySelector('.phoneFormat');
     const phone = input.value;
     const found = reqex.test(phone);
+    if (!found && phone.length) {
+        input.classList.add('invalid');
+        format.classList.add('block');
+    } else {
+        input.classList.remove('invalid');
+        format.classList.remove('block');
+    }
 })
